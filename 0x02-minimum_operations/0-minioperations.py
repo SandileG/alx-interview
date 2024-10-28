7b868acaf3775922d0f8267397e7582b501e9452
SandileG <sandilegraham@gmail.com>
2024-10-28 19:33:41 +0000

    0-minioperations.py

---
+++ 0x02-minimum_operations/0-minioperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def minOperations(n):
+    if n < 2:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    # Divide n by smallest factor and add the factor to operations count
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n /= factor
+        factor += 1
+
+    return operations
