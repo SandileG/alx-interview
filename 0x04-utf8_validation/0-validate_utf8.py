0e0a4e46d7dd10e95dc7dd0b9a8b23c777b83ce3
SandileG <sandilegraham>
2024-11-13 23:19:26 +0200

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,14 +5,15 @@ UTF-8 Validation Module
 This module provides a function to check if a given dataset represents UTF-8.
 """
 
+
 def validUTF8(data):
     num_bytes = 0  # Number of bytes in the current UTF-8 character
-    
+
     for byte in data:
         byte = byte & 0xFF  # Mask to ensure only the last 8 bits are used
-        
+
         if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character based on leading bits
+            # Determine number of bytes in UTF-8 based on leading bits
             if (byte >> 5) == 0b110:  # 2-byte character
                 num_bytes = 1
             elif (byte >> 4) == 0b1110:  # 3-byte character
@@ -26,6 +27,5 @@ def validUTF8(data):
             if (byte >> 6) != 0b10:
                 return False
             num_bytes -= 1
-    
-    # If num_bytes is not zero, it means there are incomplete characters
+
     return num_bytes == 0
